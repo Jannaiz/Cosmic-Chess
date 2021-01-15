9c46a72693e394b033b86ef7173c5dc72edee4ae
AtlantiaKing <dekeukelaeresander@gmail.com>
2021-01-15 18:13:57 +0100

    15/01/2020 Sander : Camera Movement
    First models and camera movement

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Camera cam;
+
+    [SerializeField] private float ySpeed;
+    [SerializeField] private float xSpeed;
+
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float maxZoom;
+
+    private bool rotating = false;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (!Physics.Raycast(ray, out hit))
+            {
+                rotating = true;
+            } else
+            {
+                if(!hit.transform.gameObject.GetComponent<Piece>())
+                {
+                    rotating = true;
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            rotating = false;
+        }
+        if (transform.localPosition.z + Input.mouseScrollDelta.y * zoomSpeed * zoomSpeed <= maxZoom)
+        {
+            transform.localPosition += new Vector3(0, 0, Input.mouseScrollDelta.y * zoomSpeed);
+        }
+        if (rotating)
+        {
+            if (transform.parent.eulerAngles.x - Input.GetAxis("Mouse Y") * xSpeed > 271f)
+            {
+                transform.parent.rotation = Quaternion.Euler(transform.parent.eulerAngles + new Vector3(-Input.GetAxis("Mouse Y") * xSpeed, 0, 0));
+            }
+            else if (transform.parent.eulerAngles.x - Input.GetAxis("Mouse Y") * xSpeed < 89f)
+            {
+                transform.parent.rotation = Quaternion.Euler(transform.parent.eulerAngles + new Vector3(-Input.GetAxis("Mouse Y") * xSpeed, 0, 0));
+            }
+
+            transform.parent.rotation = Quaternion.Euler(transform.parent.eulerAngles + new Vector3(0, Input.GetAxis("Mouse X") * ySpeed, 0));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+    }
+}
