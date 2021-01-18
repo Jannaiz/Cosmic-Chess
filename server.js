8b7c67b8a5466af2eec287a8c27ead47021bdcc5
Jannes Vanquaillie <36005485+Jannaiz@users.noreply.github.com>
2021-01-18 21:38:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws');
 const { v4: uuidv4 } = require('uuid');
 
 const server = http.createServer();
-const port = procces.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const wss = new WebSocket.Server({ noServer: true, clientTracking: true});
 
