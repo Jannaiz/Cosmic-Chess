f27e7d15dd0ae1d98475789e8b01d05ccbf4668e
Jannes Vanquaillie <36005485+Jannaiz@users.noreply.github.com>
2021-01-19 20:50:01 +0100

    development

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const http = require('http');
 const url = require('url');
 const WebSocket = require('ws');
 const { v4: uuidv4 } = require('uuid');
+const Buffer = require('buffer').Buffer;
 
 const server = http.createServer();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const wss = new WebSocket.Server({ noServer: true, clientTracking: true});
+
+
 
 
 
@@ -25,15 +28,19 @@ wss.on('connection', function connection(ws, request) {
 
   ws.send(JSON.stringify(sessionMsg));
 
+[]
+  ws.on('message', function(data) {
+    console.log('Message: '+data);
 
-  ws.on('message', function(msgString) {
-    console.log('Message: '+msgString);
-
+    const buf = Buffer.from(data);
+    console.log(data.readInt32BE(0));
+    console.log(data.readInt32BE(4));
     //ws.send(msgString);
 
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(msgString);
+
+        client.send(data);
       }
     });
 
