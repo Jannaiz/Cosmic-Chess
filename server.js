beef9b638c906f62d064aa6e876b725c072d2ddb
Jannes Vanquaillie <36005485+Jannaiz@users.noreply.github.com>
2021-01-22 23:46:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -92,26 +92,66 @@ wss.on('connection', function connection(ws, request) {
       case 3:
       // recive: joinRequest	3:	username, lobbyCode
       // send: 3 succes (0 of 1)
-
-
-
-
-
         var pakket = makePacket(3);
+
+        var username = data.username;
+        var lobbyCode = data.lobbyCode;
+
+        lobbys.getData(lobbyCode).addUser(username,users.getData(username));
+
+        pakket.succes = 1;
+
+        //console.log(users.getData(username));
+        //users.getData(username).ws.send("hello");
+
+
+
 
         break;
       case 4:
-      // recive: Handshake	1:	username
-      // send: 1 sessionId("id"), welcomeMessage
+      // recive: ReadyUp	4:	lobbyCode,username
+      // send: 1 color
 
-        var pakket = makePacket(4);
+
+
+        var username = data.username;
+        users.getData(username).readyState = true;
+        lobbys.getData(users.getData(username).lobbyCode).setReady(username,true);
+
+        if(lobbys.getData(users.getData(username).lobbyCode).checkAllReady){
+
+          console.log("All users are ready for the game of "+users.getData(username).lobbyCode);
+
+          var pakket = makePacket(4);
+          var lobbyUsers = lobbys.getData(users.getData(username).lobbyCode).getAllUsers();
+
+          console.log(lobbyUsers);
+
+          for (var i = 0; i < lobbyUsers.length; i++) {
+            pakket.color = i;
+            console.log(lobbyUsers[i]);
+            lobbyUsers[i][0][1].ws.send(JSON.stringify(pakket));
+
+          }
+        }
+        var pakket = null;
+
 
         break;
       case 5:
-      // recive: Handshake	1:	username
-      // send: 1 sessionId("id"), welcomeMessage
+      // recive: Movment	5:	startPos, endPos
+      // send: 5 startPos, endPos
 
-        var pakket = makePacket(5);
+        var pakket = null;
+        var username = data.username;
+
+        var clients = lobbys.getData(users.getData(username)).getAllUsers();
+
+        clients.forEach(function each(client) {
+          if (client.ws !== ws && client.ws.readyState === WebSocket.OPEN) {
+            client.ws.send(data);
+          }
+        });
 
 
 
@@ -132,7 +172,7 @@ wss.on('connection', function connection(ws, request) {
           lobbys.addData(code, newLobby);
 
           pakket.lobbyCode = code;
-
+          users.getData(username).lobbyCode = code;
           break;
 
       default:
@@ -140,8 +180,10 @@ wss.on('connection', function connection(ws, request) {
         var pakket = makePacket("-1");
 
     }
+    if(pakket){
+      ws.send(JSON.stringify(pakket));
+    }
 
-    ws.send(JSON.stringify(pakket));
 
 
     /*if( Number(code) == 0){
@@ -207,6 +249,6 @@ server.listen(port, function() {
 
 function makePacket(pakketType) {
   return pakket = {
-    packetType:pakketType
+    pakketType:pakketType
   };
 }
